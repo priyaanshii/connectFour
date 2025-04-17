a7568a504fade6399166f8318126f5870d6a3a6e
Birinder SINGH <birinder.singh@amadeus.com>
2025-04-18 00:16:13 +0530

    fix(board): Diagonal logic fixed

--- board.py
+++ board.py
@@ -1,21 +1,22 @@
 from player import Player
 
+
 class Board():
     def __init__(self, height, width, players: list[Player]):
         self.height = height
         self.width = width
         self.players = players
         self.turn = self.players[0]
-        self.board= [[" "] * self.width for _ in range(self.height)]
+        self.board = [[" "] * self.width for _ in range(self.height)]
         self.colHeight = [self.height - 1] * self.width
 
     def draw(self):
 
         # Draw the opening line
-        print("╔", end = "")
+        print("╔", end="")
 
         for _ in range(self.width - 1):
-            print("═══╦", end = "")
+            print("═══╦", end="")
         print("═══╗")
 
         # Draw the actual cells w/ content
@@ -23,18 +24,18 @@ class Board():
         left, bottom, right = "╠", "═══╬", "═══╣"
         for y in range(self.height):
             # Content
-            print("║", end = "")
+            print("║", end="")
             for x in range(self.width):
-                print(f" {self.board[y][x]} ║", end = "")
+                print(f" {self.board[y][x]} ║", end="")
             print()
 
             if y == self.height - 1:
                 left, bottom, right = "╚", "═══╩", "═══╝"
 
             # Roof for next content
-            print(left, end = "")
+            print(left, end="")
             for x in range(self.width-1):
-                print(bottom, end = "")
+                print(bottom, end="")
             print(right)
 
     def changeTurn(self):
@@ -48,7 +49,7 @@ class Board():
             return False
         if self.colHeight[curCol] < 0:
             return False
-        
+
         return True
 
     def putPiece(self, curCol):
@@ -61,49 +62,49 @@ class Board():
             if self.board[self.colHeight[curCol]+i+1][curCol] != self.turn.symbol:
                 return False
         return True
-    
+
     def verifyHorizontal(self, curCol):
-        count=0
+        count = 0
         for i in range(self.width):
             if self.board[self.colHeight[curCol]][i] != self.turn.symbol:
                 count = 0
             else:
-                count+=1
+                count += 1
                 if count == 4:
                     return True
         return False
-    
+
     def verifyDownDBir(self, curCol):
         x = curCol
         y = self.colHeight[curCol]
 
         if y < x:
-            tly, tlx = 0, y-x
+            tly, tlx = 0, x-y
         else:
-            tly, tlx = x-y, 0
-        
+            tly, tlx = y-x, 0
+
         i = 0
         ct = 0
         while tly + i < self.height and tlx + i < self.width:
-            if self.board[tly+i][tly+i] != self.turn.symbol:
+            if self.board[tly+i][tlx+i] != self.turn.symbol:
                 ct = 0
             else:
                 ct += 1
                 if ct == 4:
                     return True
             i += 1
-        
+
         return False
-    
+
     def verifyUpDBir(self, curCol):
         x = curCol
         y = self.colHeight[curCol]
 
-        if self.height - y < x:
-            tly, tlx = self.height - 1, x - (self.height - y)
+        if self.height - y - 1 < x:
+            tly, tlx = self.height - 1, x - (self.height - y - 1)
         else:
-            tly, tlx = y+x-1, 0
-        
+            tly, tlx = y+x, 0
+
         i = 0
         ct = 0
         while tly - i >= 0 and tlx + i < self.width:
@@ -114,11 +115,11 @@ class Board():
                 if ct == 4:
                     return True
             i += 1
-        
+
         return False
 
     def verifyDiagonalBir(self, curCol):
-        return self.verifyDownDBir(curCol) or self.verifyUpDBir(curCol)        
+        return self.verifyDownDBir(curCol) or self.verifyUpDBir(curCol)
 
     def verifyDiagonal(self, curCol):
         ...
@@ -130,9 +131,9 @@ class Board():
         self.putPiece(curCol)
         if self.verifyWin(curCol):
             return True
-        self.colHeight[curCol]-=1
+        self.colHeight[curCol] -= 1
         return False
-        
+
 
 # board = Board(7, 6, (Player("X"), Player("Y")))
 # board.colHeight[2] = 1
--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import os
 from player import Player
 from board import Board
 
+
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def main() :
+
+def main():
     p1 = Player(symbol="X")
     p2 = Player(symbol="Y")
-    board = Board(6,7,[p1,p2])
+    board = Board(6, 7, [p1, p2])
 
     while True:
         while True:
@@ -30,5 +32,6 @@ def main() :
             print(f"Player {board.turn} has won!")
             break
         board.changeTurn()
+
 
 main()
